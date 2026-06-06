127e79a923e0913ff0a372b3094b0c666e63eb07
刘永康 <15994847+kang01037@user.noreply.gitee.com>
2026-06-06 10:54:21 +0800

    初始化

---
+++ src/main/java/org/example/railwayreal/RailwayRealApplication.java
@@ -0,0 +1,13 @@
+package org.example.railwayreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayRealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayRealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/railwayreal/RailwayRealApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.railwayreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayRealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
